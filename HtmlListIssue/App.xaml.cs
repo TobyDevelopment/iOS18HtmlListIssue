ebd172ec801d7a9d1e8f440d555492148ff41f04
TobyDevelopment <74288784+TobyDevelopment@users.noreply.github.com>
2025-02-18 13:42:56 +1100

    Add files via upload

---
+++ HtmlListIssue/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HtmlListIssue;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ HtmlListIssue/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HtmlListIssue;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        
+        this.unorderedList.Text = "<ul><li>item 1</li><li>item 2</li><li>item 3</li></ul>";
+        this.orderedList.Text = "<ol><li>item 1</li><li>item 2</li><li>item 3</li></ol>";
+    }
+}
